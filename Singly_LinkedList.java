47924bb40cc5a8986b8916aaaafa7384b9f2fa1e
jenaishu17 <143867693+jenaishu17@users.noreply.github.com>
2025-03-19 10:20:08 +0530

    Add files via upload

--- Singly_LinkedList.java
+++ Singly_LinkedList.java
@@ -4,11 +4,18 @@ public class Singly_LinkedList {
 
 	public static void main(String args[]) {
 		Linked_ex l=new Linked_ex();
-		l.insertAtBegin(45);
-		l.insertAtBegin(62);
-		l.insertAtBegin(02);
+		l.insertAtBegin(60);
+		l.insertAtBegin(50);
+		l.insertAtBegin(40);
+		l.insertAtBegin(30);
+		l.insertAtBegin(20);
+		l.insertAtBegin(10);
+		l.insertAtBegin(777);
 		l.display();
-	
+		l.insertAtAnyPos(4, 333);
+		l.display();
 	}
+	
+	
 }
 
